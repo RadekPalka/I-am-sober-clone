bd9e7828ccd3837ee9b78e454cdc66582c9dac81
Radoslaw Palka <radekpalka@gmail..com>
2024-05-29 12:49:25 +0200

    set deafauld addiction date to input in main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const addictionTypeSelect = document.querySelector("#addiction-type")
+const addictionFreeDateInput = document.querySelector("#addiction-free")
 
+const defaultDate = new Date().toLocaleDateString('en-CA',{ year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll("/", "-")
+console.log(defaultDate)
+addictionFreeDateInput.value = defaultDate
 addictionTypeSelect.addEventListener("change", e=>{
   console.log(e.target.value)
+})
+
+addictionFreeDateInput.addEventListener("change", e=>{
+  console.log(e.target.value, typeof e.target.value)
 })
